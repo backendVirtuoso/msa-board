0056ca1cb4a40e59f3dbd7322fbc5736b0767406
backendVirtuoso <129765568+backendVirtuoso@users.noreply.github.com>
2025-02-16 21:20:44 +0900

    [섹션 4] 좋아요 구현

---
+++ service/like/src/main/java/msa/board/like/service/ArticleLikeService.java
@@ -0,0 +1,40 @@
+package msa.board.like.service;
+
+import lombok.RequiredArgsConstructor;
+import msa.board.common.snowflake.Snowflake;
+import msa.board.like.entity.ArticleLike;
+import msa.board.like.repository.ArticleLikeRepository;
+import msa.board.like.service.response.ArticleLikeResponse;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+public class ArticleLikeService {
+    private final Snowflake snowflake = new Snowflake();
+    private final ArticleLikeRepository articleLikeRepository;
+
+    public ArticleLikeResponse read(Long articleId, Long userId) {
+        return articleLikeRepository.findByArticleIdAndUserId(articleId, userId)
+                .map(ArticleLikeResponse::from)
+                .orElseThrow();
+    }
+
+    @Transactional
+    public void like(Long articleId, Long userId) {
+        articleLikeRepository.save(
+                ArticleLike.create(
+                        snowflake.nextId(),
+                        articleId,
+                        userId
+                )
+        );
+    }
+
+    @Transactional
+    public void unlike(Long articleId, Long userId) {
+        articleLikeRepository.findByArticleIdAndUserId(articleId, userId)
+                .ifPresent(articleLikeRepository::delete);
+    }
+
+}
