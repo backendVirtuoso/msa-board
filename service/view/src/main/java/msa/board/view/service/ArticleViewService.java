210fe66968a57bbd770431fe5f664cc94014344f
backendVirtuoso <129765568+backendVirtuoso@users.noreply.github.com>
2025-02-18 22:57:02 +0900

    [섹션 5] 조회수 어뷰징 방지 정책 구현

--- service/view/src/main/java/msa/board/view/service/ArticleViewService.java
+++ service/view/src/main/java/msa/board/view/service/ArticleViewService.java
@@ -2,19 +2,29 @@ package msa.board.view.service;
 
 import lombok.RequiredArgsConstructor;
 import msa.board.view.repository.ArticleViewCountRepository;
+import msa.board.view.repository.ArticleViewDistributedLockRepository;
 import org.springframework.stereotype.Service;
+
+import java.time.Duration;
 
 @Service
 @RequiredArgsConstructor
 public class ArticleViewService {
     private final ArticleViewCountRepository articleViewCountRepository;
     private final ArticleViewCountBackUpProcessor articleViewCountBackUpProcessor;
+    private final ArticleViewDistributedLockRepository articleViewDistributedLockRepository;
+
     private static final int BACK_UP_BATCH_SIZE = 100;
+    private static final Duration TTL = Duration.ofMinutes(10);
 
     public Long increase(Long articleId, Long userId) {
+        if (!articleViewDistributedLockRepository.lock(articleId, userId, TTL)) {
+            return articleViewCountRepository.read(articleId);
+        }
+
         Long count = articleViewCountRepository.increase(articleId);
         if (count % BACK_UP_BATCH_SIZE == 0) {
-            articleViewCountBackUpProcessor.backUp(articleId, userId);
+            articleViewCountBackUpProcessor.backUp(articleId, count);
         }
         return count;
     }
--- service/view/src/test/java/msa/board/view/api/ViewApiTest.java
+++ service/view/src/test/java/msa/board/view/api/ViewApiTest.java
@@ -18,8 +18,9 @@ public class ViewApiTest {
         for (int i = 0; i < 10000; i++) {
             executorService.submit(() -> {
                 restClient.post()
-                        .uri("/v1/article-views/articles/{articleId}/users/{userId}", 3L, 1L)
-                        .retrieve();
+                        .uri("/v1/article-views/articles/{articleId}/users/{userId}", 4L, 1L)
+                        .retrieve()
+                        .toBodilessEntity();
                 latch.countDown();
             });
         }
@@ -27,11 +28,10 @@ public class ViewApiTest {
         latch.await();
 
         Long count = restClient.get()
-                .uri("/v1/article-views/articles/{articleId}/count", 3L)
+                .uri("/v1/article-views/articles/{articleId}/count", 4L)
                 .retrieve()
                 .body(Long.class);
 
         System.out.println("count = " + count);
-
     }
 }
