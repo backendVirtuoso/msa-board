9f7b0be0a3231d3d5a56f0bed4608500fd4fc945
backendVirtuoso <129765568+backendVirtuoso@users.noreply.github.com>
2025-02-24 00:09:10 +0900

    [섹션 7] 캐시 최적화 전략 구현 - Request Collapsing

--- service/article-read/src/main/java/msa/board/articleread/client/ViewClient.java
+++ service/article-read/src/main/java/msa/board/articleread/client/ViewClient.java
@@ -3,6 +3,7 @@ package msa.board.articleread.client;
 import jakarta.annotation.PostConstruct;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import msa.board.articleread.cache.OptimizedCacheable;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Component;
@@ -25,7 +26,8 @@ public class ViewClient {
     // Redis에서 데이터를 조회해본다.
     // 레디스에 데이터가 없었다면, count 메서드 내부 로직이 호출되면서, viewService로 원본 데이터를 요청한다. 그리고, 레디스에 데이터를 넣고 응답한다.
     // 레디스에 데이터가 있었다면, 그 데이터를 그대로 반환한다.
-    @Cacheable(key = "#articleId", value = "articleViewCount")
+//    @Cacheable(key = "#articleId", value = "articleViewCount")
+    @OptimizedCacheable(type = "articleViewCount", ttlSeconds = 1)
     public long count(Long articleId) {
         log.info("[ViewClient.count] articleId={}", articleId);
 
